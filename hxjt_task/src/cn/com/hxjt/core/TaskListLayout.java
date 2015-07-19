f87f68c5901ddb2989a0423c021f63848237384f
pangjianhai <821761509@qq.com>
2015-07-19 20:16:55 +0800

    tasks

--- hxjt_task/src/cn/com/hxjt/core/TaskListLayout.java
+++ hxjt_task/src/cn/com/hxjt/core/TaskListLayout.java
@@ -1,5 +1,14 @@
 package cn.com.hxjt.core;
 
-public class TaskListLayout extends BaseActivity {
+import android.os.Bundle;
+import android.view.Window;
 
+public class TaskListLayout extends BaseActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.tasks_list_layout);
+
+	}
 }
