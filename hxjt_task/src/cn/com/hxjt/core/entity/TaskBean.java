dde550272534cff6f601ac5ead9725e82d55559e
pangjianhai <821761509@qq.com>
2015-10-10 09:10:00 +0800

    detail

--- hxjt_task/src/cn/com/hxjt/core/entity/TaskBean.java
+++ hxjt_task/src/cn/com/hxjt/core/entity/TaskBean.java
@@ -2,4 +2,19 @@ package cn.com.hxjt.core.entity;
 
 public class TaskBean {
 
+	private String id;
+	private String name;
+	private String pro;
+	private String proPosition;
+	private String runningState;
+	private String arranger;
+	private String receiver;
+	private String creator;
+	private String createDate;
+	private String completeDate;
+	private String importantLevel;
+	private String emergentLevel;
+	private String requireCompleteDate;
+	private boolean isNeedCheck;
+
 }
