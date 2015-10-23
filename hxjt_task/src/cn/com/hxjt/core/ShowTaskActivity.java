3dca5dd4a9e676c0ade42e71c1595321e8950488
pangjianhai <821761509@qq.com>
2015-10-23 11:25:22 +0800

    detail

--- hxjt_task/src/cn/com/hxjt/core/ShowTaskActivity.java
+++ hxjt_task/src/cn/com/hxjt/core/ShowTaskActivity.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -116,6 +117,10 @@ public class ShowTaskActivity extends ParentTaskActivity {
 		completionDate.setText(ct);
 		importantLevel.setText(tb.getImportantLevel());
 		emergentLevel.setText(tb.getEmergentLevel());
+	}
+
+	public void ops_task(View v) {
+
 	}
 
 }
